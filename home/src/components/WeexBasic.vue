90a57eb0927fa6e79563ebc5ff8ce6c772266d4d
apple <839864600@qq.com>
2019-03-20 13:11:43 +0800

    d

---
+++ home/src/components/WeexBasic.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="body" @onclick="testfuncion">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeexBasic',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    testfuncion: () => {
+      console.log('testfunciontestfunciontestfuncion')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.body {
+  background-color: #f2f2f2;
+}
+</style>
--- home/src/router/index.js
+++ home/src/router/index.js
@@ -2,19 +2,26 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Main from '@/components/Main'
+import WeexBasic from '@/components/WeexBasic'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      redirect: '/main'
+    },
+    {
+      path: '/main',
       name: 'Main',
       component: Main
     },
     {
-      path: '/helloworld',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/weexbasic',
+      name: 'WeexBasic',
+      component: WeexBasic
     }
   ]
 })
